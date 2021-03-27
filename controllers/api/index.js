a0bf61559537cc499649fefb25a9a5433fdf0cc3
The Noise <leespaulding25@gmail.com>
2021-03-27 11:41:19 -0400

    added mentor routes log in log out

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
+const mentorRoutes = require("./mentorRoutes");
 
 router.use("/users", userRoutes);
+router.use("/mentors",mentorRoutes);
 
 module.exports = router;
