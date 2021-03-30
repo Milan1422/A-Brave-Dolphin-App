e8ef7bb8499e28190c553edbbb7cd352dcd92c12
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-29 22:01:31 -0400

    grabbed text from chatbox and sent it to the server

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,17 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io();
 
 socket.on('message', message => {
     console.log(message);
+})
+
+// message submit
+chatForm.addEventListener('submit', (e) => {
+    // prevent page from refresh after submit
+    e.preventDefault();
+    // grabs the text from chatbox
+    const msg = e.target.elements.msg.value;
+    // emits msg to server
+    socket.emit('chatMessage', msg);
 })
