ae105976f9d813579b05a9aeeb87fae484dae945
zarriei <75182441+zarriei@users.noreply.github.com>
2021-03-30 22:06:36 -0400

    mentor handlebars, login handlebars, signup handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -31,6 +31,17 @@ router.get("/profile", withAuth, async (req, res) => {
   }
 });
 
+router.get("/signup", async (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("signUp");
+ 
+});
+
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
