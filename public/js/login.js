cbe588313209c7fc67f48ac823e0fa305c7de8c8
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-31 23:17:05 -0400

    working on signup route

--- public/js/login.js
+++ public/js/login.js
@@ -25,14 +25,16 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
+  const firstName = document.querySelector('#first-name-signup').value.trim();
+  const lastName = document.querySelector('#last-name-signup').value.trim();
+  const username = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
   if (name && email && password) {
-    const response = await fetch('/api/users', {
+    const response = await fetch('/api/youths', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ firstName, lastName, username, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
