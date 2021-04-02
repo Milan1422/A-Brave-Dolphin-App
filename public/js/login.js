08966dfaa990809c63ba8dd9f1d69ce5f748f4cf
anthonydominguez86 <adominguez11186@gmail.com>
2021-04-01 22:57:01 -0400

    Jacks Update

--- public/js/login.js
+++ public/js/login.js
@@ -22,39 +22,7 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const firstName = document.querySelector('#first-name-signup').value.trim();
-  const lastName = document.querySelector('#last-name-signup').value.trim();
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (name && email && password) {
-    const response = await fetch('/api/youths', {
-      method: 'POST',
-      body: JSON.stringify({ firstName, lastName, username, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
-    }
-  }
-    
-
-  
-};
-
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
-
-
-document
-  .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
 
