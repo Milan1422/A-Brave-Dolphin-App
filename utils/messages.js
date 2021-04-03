b773975b8e23ed3c0163687d153ed49241da8149
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-04-03 08:12:15 -0400

    fixed time and hardcoded user in chatbox

---
+++ utils/messages.js
@@ -0,0 +1,12 @@
+// format message box to display actual name and time
+const moment = require('moment');
+
+function formatMessage(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format('h:mm a')
+  };
+}
+
+module.exports = formatMessage;
